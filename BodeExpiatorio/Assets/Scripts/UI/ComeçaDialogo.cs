e7c4e3481dc6d8c8b39cb8edc083a8471c5879e6
SilvioMartinezz <131674008+SilvioMartinezz@users.noreply.github.com>
2024-10-30 09:36:49 -0300

    .

--- BodeExpiatorio/Assets/Scripts/UI/ComeçaDialogo.cs
+++ BodeExpiatorio/Assets/Scripts/UI/ComeçaDialogo.cs
@@ -10,16 +10,16 @@ public class ComecaDialogo : MonoBehaviour
         public GameObject objetoParaAtivar;
     }
 
-    public List<ParObjetos> paresDeObjetos; // Lista de pares de objetos para verificar e ativar
+    public List<ParObjetos> paresDeObjetos; 
 
     void Update()
     {
         foreach (ParObjetos par in paresDeObjetos)
         {
-            // Verifica se o objeto do par está inativo
+            
             if (par.objetoParaVerificar != null && !par.objetoParaVerificar.activeInHierarchy)
             {
-                // Ativa o objeto correspondente do par, caso esteja inativo
+                
                 if (par.objetoParaAtivar != null && !par.objetoParaAtivar.activeInHierarchy)
                 {
                     par.objetoParaAtivar.SetActive(true);
