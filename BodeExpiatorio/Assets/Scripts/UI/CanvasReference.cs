7bdac3120da5c20253ac404525df729bac8e3e0b
SilvioMartinezz <131674008+SilvioMartinezz@users.noreply.github.com>
2024-11-11 05:14:24 -0300

    Dialogo novo

---
+++ BodeExpiatorio/Assets/Scripts/UI/CanvasReference.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasReference : MonoBehaviour
+{
+    public Image image;
+    public TextMeshProUGUI tmp;
+    public AudioSource audioSource;
+    public Canvas canvas;
+}
