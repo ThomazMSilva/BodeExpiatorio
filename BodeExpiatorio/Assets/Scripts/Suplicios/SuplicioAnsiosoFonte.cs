abef4a1c3bd2bee87b5119e727989fd5ef5cdf59
RektvmSlvg <thomazm.silva@gmail.com>
2024-11-18 23:20:15 -0300

    corrigindo bug succ

--- BodeExpiatorio/Assets/Scripts/Suplicios/SuplicioAnsiosoFonte.cs
+++ BodeExpiatorio/Assets/Scripts/Suplicios/SuplicioAnsiosoFonte.cs
@@ -6,8 +6,8 @@ public class SuplicioAnsiosoFonte : MonoBehaviour
     [SerializeField] private bool canSpitOut = true;
     [SerializeField] private float spitDamage = 10;
     [SerializeField] private float spitForce = 15;
-    private bool hasBindPlayer;
-    private bool isPlayerInside;
+    [SerializeField] private bool hasBindPlayer;
+    [SerializeField] private bool isPlayerInside;
     private Vector3 spitDirection;
     private Jogador _player;
 
@@ -15,49 +15,55 @@ public class SuplicioAnsiosoFonte : MonoBehaviour
     {
         suplicioEfeito.OnChangedAttractingState += AttractingStateHasChanged;
         spitDirection.Set(transform.right.x, .5f, 0);
+
+        _player = FindAnyObjectByType<Jogador>();
+        _player.Vida.OnPlayerDeath += SetPlayerOutside;
     }
 
     private void AttractingStateHasChanged()
     {
         if (!hasBindPlayer)
-        { 
-            if(isPlayerInside && suplicioEfeito.IsAttracting) BindPlayer();
+        {
+            if (isPlayerInside && suplicioEfeito.IsAttracting) BindPlayer();
+
             return; 
         }
         
-        Debug.Log("Destravou o paer");
         _player.Movimento.SetPlayerBind(false);
         hasBindPlayer = false;
-        
-        if(canSpitOut)
+
+        if (canSpitOut)
+        {
             _player.ApplyDamageEffect(spitDamage, spitDirection * spitForce, 2, this);
+            isPlayerInside = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //POR QUE TA COLIDINDO 25 BILHOES DE VEZES POR FRAME.???????
-        if (other.gameObject.CompareTag("Player")) 
-        {
+        if (other.gameObject.CompareTag("Player"))
+        { 
             isPlayerInside = true;
-
-            if(_player == null) //por enqnt deixei assim. N funcionaria com multiplayer.
-                _player = other.GetComponent<Jogador>();
-        }
-
-        if (!suplicioEfeito.IsAttracting) return;
         
-        if (!hasBindPlayer) BindPlayer();
+            if (!suplicioEfeito.IsAttracting) return;
+
+            if (!hasBindPlayer) BindPlayer();
+        }
+        
         
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) isPlayerInside = false;
+        if (other.gameObject.CompareTag("Player")) SetPlayerOutside();
     }
+
+    private void SetPlayerOutside() => isPlayerInside = false;
 
     private void BindPlayer()
     {
-        _player.SetPosition(transform.position);
+        //_player.SetPosition(transform.position);
         _player.Movimento.SetPlayerBind(true);
         hasBindPlayer = true;
     }
