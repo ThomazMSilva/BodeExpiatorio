4f03bfbc253bb7c166ab258944f049cc933d074e
RektvmSlvg <thomazm.silva@gmail.com>
2024-11-26 01:30:09 -0300

    update menus + dialogos + desejo1

--- BodeExpiatorio/Assets/Scripts/UI/MenuPrincipalManager.cs
+++ BodeExpiatorio/Assets/Scripts/UI/MenuPrincipalManager.cs
@@ -1,9 +1,10 @@
-using Newtonsoft.Json.Bson;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class MenuPrincipalManager : MonoBehaviour
 {
+    [SerializeField] private UINavigationManager navigationManager;
+
     [SerializeField] private GameObject pauseMenuScreen;
     [SerializeField] private GameObject mainMenuScreen;
     [SerializeField] private GameObject optionsScreen;
@@ -12,119 +13,51 @@ public class MenuPrincipalManager : MonoBehaviour
     [SerializeField] private GameObject soundScreen;
     [SerializeField] private GameObject accessiblityScreen;
 
-
-    [SerializeField] private GameObject firstMainMenuOption;
-    [SerializeField] private GameObject firstPauseMenuOption;
-    [SerializeField] private GameObject firstConfigOption;
-    [SerializeField] private GameObject creditsReturnBTN;
-    [SerializeField] private GameObject firstGraphicsBTN;
-    [SerializeField] private GameObject firstSoundBTN;
-    [SerializeField] private GameObject firstAccessibilityBTN;
-
-    private bool isMainMenu;
-
-    public void SetOptionsSelected() => EventSystem.current.SetSelectedGameObject(firstConfigOption);
-    public void SetPauseMenuSelected() => EventSystem.current.SetSelectedGameObject(firstPauseMenuOption);
-    public void SetMainSelected() => EventSystem.current.SetSelectedGameObject(firstMainMenuOption);
-    public void SetGraphicsSelected() => EventSystem.current.SetSelectedGameObject(firstGraphicsBTN);
-    public void SetSoundsSelected() => EventSystem.current.SetSelectedGameObject(firstSoundBTN);
-    public void SetCreditsSelected() => EventSystem.current.SetSelectedGameObject(creditsReturnBTN);
-    public void SetAccessibilitySelected() => EventSystem.current.SetSelectedGameObject(firstAccessibilityBTN);
-
-    private void Start() => isMainMenu = mainMenuScreen.activeSelf;
-
     public void AbrirMenuGrafico()
     {
-        optionsScreen.SetActive(false);
-        graphicsScreen.SetActive(true);
-
-        SetGraphicsSelected();
+        navigationManager.OpenPanel(graphicsScreen);
     }
 
     public void AbrirMenuSom()
     {
-        optionsScreen.SetActive(false);
-        soundScreen.SetActive(true);
-
-        SetSoundsSelected();
+        navigationManager.OpenPanel(soundScreen);
     }
 
     public void AbrirMenuAcessibilidade()
     {
-        optionsScreen.SetActive(false);
-        accessiblityScreen.SetActive(true);
-
-        SetAccessibilitySelected();
+        navigationManager.OpenPanel(accessiblityScreen);
     }
 
     public void AbrirOpções()
     {
-        if(!isMainMenu) pauseMenuScreen.SetActive(false);
-        else mainMenuScreen.SetActive(false);
-
-        optionsScreen.SetActive(true);
-
-        SetOptionsSelected();
+        navigationManager.OpenPanel(optionsScreen);
     }
 
     public void OpenCredits()
     {
-        if (!isMainMenu) pauseMenuScreen.SetActive(false);
-        else mainMenuScreen.SetActive(false);
-        creditsScreen.SetActive(true);
-        SetCreditsSelected();
+        navigationManager.OpenPanel(creditsScreen);
     }
 
     public void CloseCredits()
     {
-        creditsScreen.SetActive(false);
-        if (!isMainMenu)
-        {
-            pauseMenuScreen.SetActive(true);
-            SetPauseMenuSelected();
-        }
-        else
-        {
-            mainMenuScreen.SetActive(true);
-            SetMainSelected();
-        }
+        navigationManager.ClosePanel();
     }
 
     public void FecharOpções()
     {
-        optionsScreen.SetActive(false);
-
-        if (!isMainMenu)
-        {
-            pauseMenuScreen.SetActive(true);
-            SetPauseMenuSelected();
-        }
-        else
-        {
-            mainMenuScreen.SetActive(true);
-            SetMainSelected();
-        }
+        navigationManager.ClosePanel();
     }
     public void FecharGrafico()
     {
-        graphicsScreen.SetActive(false);
-        optionsScreen.SetActive(true);
-
-        SetOptionsSelected();
+        navigationManager.ClosePanel();
     }
     public void FecharSom()
     {
-        soundScreen.SetActive(false);
-        optionsScreen.SetActive(true);
-
-        SetOptionsSelected();
+        navigationManager.ClosePanel();
     }
     public void FecharAcessibilidade()
     {
-        accessiblityScreen.SetActive(false);
-        optionsScreen.SetActive(true);
-        
-        SetOptionsSelected();
+        navigationManager.ClosePanel();
     }
     public void ReturnToMainMenu() => GameManager.Instance.LoadMainMenu();
 
